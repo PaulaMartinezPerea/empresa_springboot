60d4c80b2a793358866602099918bc089d045a58
Paula Martinez Perea <pmarper2003@g.educaand.es>
2025-12-04 21:12:44 +0100

    Delete Empresa directory

--- Empresa/src/main/java/com/aprendec/repository/NominaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aprendec.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aprendec.model.Nomina;
-
-public interface NominaRepository extends JpaRepository<Nomina, String> {
-	
-}
